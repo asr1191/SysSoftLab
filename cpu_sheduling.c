1c59bc8c4ca3c65946129f9a24ccc7c094d5b943
Amal S Raj <amalsraj@cet.ac.in>
2017-08-04 10:02:42 +0530

    init

---
+++ cpu_sheduling.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void main(){
+
+    struct proc{
+        int pid;
+        int priority;
+        int burst;
+        struct proc* next;
+    };
+
+    
+}
